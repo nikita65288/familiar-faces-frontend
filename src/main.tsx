35d384fdd50f83f8aaa871912bdd3a635e672298
Nikita Tribunskii <nikita65288@gmail.com>
2026-04-17 15:41:29 +0300

    Fix Roboto and dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "@/app/App";
-
-console.log("main.tsx loaded");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
